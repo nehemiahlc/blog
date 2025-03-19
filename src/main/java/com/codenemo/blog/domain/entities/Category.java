fe5dade98f9d44b9079185665bc60654e706de87
nehemiah <nehemiahl.chandler@gmail.com>
2025-03-18 23:31:58 -0500

    Added Project files and User and Category Entity

---
+++ src/main/java/com/codenemo/blog/domain/entities/Category.java
@@ -0,0 +1,47 @@
+package com.codenemo.blog.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
+
+// Custom equals implementation needed to avoid Lombok's implementation
+// which can cause issues with JPA entity lifecycle management
+
+
+
+
+
+
---
+++ src/main/java/com/codenemo/blog/domain/entities/Tag.java
@@ -0,0 +1,4 @@
+package com.codenemo.blog.domain.entities;
+
+public class Tag {
+}
